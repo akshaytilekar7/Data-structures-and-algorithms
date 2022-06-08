da105588178946207ff80c523e37ecd00416f352
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-06-08 20:18:50 +0530

    Partition Array According to Given Pivot

--- Data structures/LeetCode/LinkList/2161.c
+++ Data structures/LeetCode/LinkList/2161.c
@@ -2,7 +2,8 @@
 // 2161. Partition Array According to Given Pivot
 
 /*
-
+Runtime: 1415 ms, faster than 7.14% of C online submissions for Partition Array According to Given Pivot.
+Memory Usage: 94.2 MB, less than 21.43% of C online submissions for Partition Array According to Given Pivot.
 */
 
 #include <stdio.h> 
@@ -10,117 +11,61 @@
 #include <assert.h>
 #include <string.h>
 
-struct ListNode {
-	int val;
-	struct ListNode* next;
-};
-
-void PrintList(struct ListNode* head, char* msg)
-{
-	if (head == NULL)
-		return;
-
-	if (msg)
-		printf("%s\t ", msg);
-
-	printf("%d\n", head->val);
-	PrintList(head->next, msg);
-}
-
-struct ListNode* Add(struct ListNode* head, int data)
-{
-	struct ListNode* temp = (struct ListNode*)malloc(sizeof(struct ListNode));
-	temp->val = data;
-	temp->next = NULL;
-
-	if (head == NULL)
-	{
-		head = (struct ListNode*)malloc(sizeof(struct ListNode));
-		head->val = data;
-		head->next = NULL;
-	}
-	else
-	{
-		struct ListNode* traverse = head;
-		while (traverse->next != NULL)
-		{
-			traverse = traverse->next;
-		}
-		traverse->next = temp;
-	}
-	return head;
-}
-
-int* GetIndexOfElements(int arr[], int size, int element, int* newSize)
-{
-	int* newArr = (int*)malloc(size * sizeof(int));
-	*newSize = 0;
-	int index = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (arr[i] == element)
-		{
-			newArr[index++] = i;
-		}
-	}
-
-	(*newSize) = index;
-	return newArr;
-}
-
-void Swap(int** arr, int x, int y)
-{
-	int temp = (*arr)[x];
-	(*arr)[x] = (*arr)[y];
-	(*arr)[y] = temp;
-}
-
-int* Partition(int arr[], int size, int pivot)
-{
-	for (int i = 0; i < size; i++)
-	{
-		int partitionIndex = 0;
-		if (arr[i] == pivot)
-		{
-			for (int j = 0; j < size; j++)
-			{
-				if (arr[j] <= pivot)
-				{
-					Swap(&arr, j, partitionIndex);
-					partitionIndex++;
-				}
-			}
-			Swap(&arr, partitionIndex, i);
-		}
-	}
-	return arr;
-}
-
+/*
+ Create one result array of same size.
+	Run a loop 
+		1st time all elements less that pivot
+		2nd time equal to pivot from next index.
+		3rd time greater to pivot from next index.
+	return array
+*/
 int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
 
-	*returnSize = numsSize;
-	int* resultArray = Partition(nums, numsSize, pivot);
-	for (int i = 0; i < numsSize; i++)
-		printf("after %d \n", resultArray[i]);
+	int n = numsSize;
+	*returnSize = n;
+	int* ans = (int*)malloc(numsSize * sizeof(int));
 
+	int len = 0;
+	for (int i = 0; i < n; i++) {
+		if (nums[i] < pivot) {
+			ans[len++] = nums[i];
+		}
+	}
 
-	return nums;
+	for (int i = 0; i < n; i++) {
+		if (nums[i] == pivot) {
+			ans[len++] = nums[i];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (nums[i] > pivot) {
+			ans[len++] = nums[i];
+		}
+	}
+
+	return ans;
+
 }
 
 int main(int argc, char* argv[])
 {
 	int size = 7;
-	int arr[] = { 9,12,5,10,14,3,15 };
+	int arr[] = { 9,12,5,10,14,3,10 };
 	int pivot = 10;
 	int returnSize = 0;
 
-	/*for (int i = 0; i < size; i++)
-		printf("before %d \n", arr[i]);*/
+	puts("before Start - ");
+	for (int i = 0; i < size; i++)
+		printf(" [%d] ", arr[i]);
+	puts("End \n");
 
-	int* result = pivotArray(arr, size, pivot, &returnSize);
+	int *ans =pivotArray(arr, size, pivot, &returnSize);
 
-	/*for (int i = 0; i < returnSize; i++)
-		printf("after %d \n", arr[i]);*/
+	puts("After Start - ");
+	for (int i = 0; i < size; i++)
+		printf(" [%d] ", ans[i]);
+	puts("End \n");
 
 	return 0;
 }
