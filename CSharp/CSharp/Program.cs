441fb55bb988db52d0388337e1a95b961343cabd
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-06-15 06:22:31 +0530

    C# solution added

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp.Leetcode.Stack;
+
+Console.WriteLine("Hello, World!");
+
+Stack20 stack = new Stack20();
+System.Console.WriteLine(stack.IsValid("]"));
