d9f9d813ed1742146ff0272edc149bc4964a8d71
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-07-23 13:24:00 +0530

    basic methods

--- Data structures/Graph/Graph.h
+++ Data structures/Graph/Graph.h
@@ -3,6 +3,16 @@
 #include <assert.h>
 #include <stdbool.h>
 
+#define SUCCESS 1 
+#define TRUE 1 
+#define FALSE 0 
+
+#define VertexAlreadyExist   2 
+#define EdgeAlreadyExists   3 
+#define InvalidVertex  4 
+#define InvalidEdge  5
+#define Corrupted 6 
+
 struct Graph
 {
 	int TotalVertex;
@@ -29,3 +39,20 @@ struct Graph* CreateGraph();
 struct HeadNode* CreateHeadNode();
 struct Node* CreateNode();
 
+int AddVertex(struct Graph* graph, int vertex);
+int AddEdge(struct Graph* graph, int vertexStart, int vertexEnd);
+int RemoveVertex(struct Graph* graph, int vertex);
+int RemoveEdge(struct Graph* graph, int vertexStart, int vertexEnd);
+void Print(struct Graph* graph, const char* msg);
+
+void GenericInsertVertex(struct HeadNode* prev, struct HeadNode* newNode, struct HeadNode* next);
+void GenericInsertNode(struct Node* prev, struct Node* newNode, struct Node* next);
+
+void InsertAtEndVertex(struct HeadNode* headNode, int vertex);
+void InsertAtEnd(struct Node* node, int vertex);
+
+struct Node* SearchNode(struct Node* node, int vertex);
+struct HeadNode* SearchVertex(struct HeadNode* headNode, int vertex);
+
+bool IsNodeExist(struct Node* node, int vertex);
+bool IsVertexExist(struct HeadNode* headNode, int vertex);
--- Data structures/Graph/GraphServer.c
+++ Data structures/Graph/GraphServer.c
@@ -17,8 +17,8 @@ struct HeadNode* CreateHeadNode()
 {
 	struct HeadNode* headNode = (struct HeadNode*)malloc(sizeof(struct HeadNode));
 	headNode->LinkList = (struct Node*)malloc(sizeof(struct Node));
-	headNode->Next = NULL;
-	headNode->Prev = NULL;
+	headNode->Next = headNode;
+	headNode->Prev = headNode;
 	headNode->Vertex = 0;
 	return headNode;
 }
@@ -26,11 +26,99 @@ struct HeadNode* CreateHeadNode()
 struct Node* CreateNode()
 {
 	struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-	node->Next = NULL;
-	node->Prev = NULL;
+	node->Next = node;
+	node->Prev = node;
 	node->Vertex = 0;
 	return node;
 }
 
+int AddVertex(struct Graph* graph, int vertex)
+{
+	if (IsVertexExist(graph->HeadNode, vertex))
+		return VertexAlreadyExist;
+	return 0;
+}
 
+int AddEdge(struct Graph* graph, int vertexStart, int vertexEnd)
+{
+}
 
+int RemoveVertex(struct Graph* graph, int vertex)
+{
+}
+
+int RemoveEdge(struct Graph* graph, int vertexStart, int vertexEnd)
+{
+}
+
+void Print(struct Graph* graph, const char* msg)
+{
+}
+
+bool IsVertexExist(struct HeadNode* headNode, int vertex)
+{
+	return SearchVertex(headNode, vertex);
+}
+
+struct HeadNode* SearchVertex(struct HeadNode* headNode, int vertex)
+{
+	struct HeadNode* traverse = headNode;
+
+	while (traverse != headNode)
+	{
+		if (traverse->Vertex == vertex)
+			return traverse;
+
+		traverse = traverse->Next;
+	}
+	return NULL;
+}
+
+void GenericInsertVertex(struct HeadNode* prev, struct HeadNode* newNode, struct HeadNode* next)
+{
+	newNode->Next= next;
+	newNode->Prev = prev;
+	prev->Next = newNode;
+	next->Prev= newNode;
+}
+
+void InsertAtEndVertex(struct HeadNode* headNode, int vertex)
+{
+	struct HeadNode* newNode = CreateHeadNode();
+	newNode->Vertex = vertex;
+	GenericInsertVertex(headNode->Prev, newNode, headNode);
+}
+
+bool IsNodeExist(struct Node* node, int vertex)
+{
+	return SearchNode(node, vertex) == NULL;
+}
+
+struct Node* SearchNode(struct Node* node, int vertex)
+{
+	struct Node* traverse = node;
+
+	while (traverse != node)
+	{
+		if (traverse->Vertex == vertex)
+			return traverse;
+
+		traverse = traverse->Next;
+	}
+	return NULL;
+}
+
+void GenericInsertNode(struct Node* prev, struct Node* newNode, struct Node* next)
+{
+	newNode->Next = next;
+	newNode->Prev = prev;
+	prev->Next = newNode;
+	next->Prev = newNode;
+}
+
+void InsertAtEnd(struct Node* node, int vertex)
+{
+	struct Node* newNode = CreateNode();
+	newNode->Vertex = vertex;
+	GenericInsertNode(node->Prev, newNode, node);
+}
