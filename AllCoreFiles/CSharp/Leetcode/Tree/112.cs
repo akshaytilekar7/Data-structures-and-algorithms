7ff510e2e01ab55e4eea6cb7fbb918592603041b
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-08-09 19:11:13 +0530

    tree 2 problems

---
+++ AllCoreFiles/CSharp/Leetcode/Tree/112.cs
@@ -0,0 +1,56 @@
+﻿// https://leetcode.com/problems/path-sum/submissions/
+// 112. Path Sum
+// https://leetcode.com/problems/path-sum/discuss/2400620/C-DFS-RECUSIVE
+
+/*
+Runtime: 152 ms, faster than 48.08% of C# online submissions for Path Sum.
+Memory Usage: 40.2 MB, less than 90.79% of C# online submissions for Path Sum.
+*/
+
+
+namespace CSharp112
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            return HasPathSumHelper(root, 0, sum);
+        }
+
+        public bool HasPathSumHelper(TreeNode root, int sum, int targetSum)
+        {
+            if (root == null)
+                return false;
+
+            sum += root.val;
+
+            if (sum == targetSum && root.left == null && root.right == null)
+                return true;
+
+            bool x = HasPathSumHelper(root.left, sum, targetSum);
+            if (x) return true;
+            bool y = HasPathSumHelper(root.right, sum, targetSum);
+            if (y) return true;
+            return false;
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            TreeNode node = new TreeNode(1);
+            node.left = new TreeNode(2);
+            var res = node.HasPathSum(node, 1);
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ AllCoreFiles/CSharp/Leetcode/Tree/113.cs
@@ -0,0 +1,64 @@
+﻿// https://leetcode.com/problems/path-sum-ii/submissions/
+// 113. Path Sum II
+// https://leetcode.com/problems/path-sum-ii/discuss/2402478/C-DFS-RECUSRIVE
+
+/*
+Runtime: 148 ms, faster than 96.77% of C# online submissions for Path Sum II.
+Memory Usage: 51.7 MB, less than 5.91% of C# online submissions for Path Sum II.
+*/
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+public class TreeNodeService
+{
+    public IList<IList<int>> PathSum(TreeNode root, int targetSum)
+    {
+        IList<IList<int>> mainLst = new List<IList<int>>();
+        if (root == null) return mainLst;
+        PathSum(root, targetSum, new List<int>(), mainLst);
+        return mainLst;
+    }
+
+    public void PathSum(TreeNode root, int targetSum, List<int> lst, IList<IList<int>> mainLst)
+    {
+        lst.Add(root.val);
+
+        if (root.left == null && root.right == null && lst.Sum() == targetSum)
+            mainLst.Add(lst);
+
+        if (root.left != null)
+            PathSum(root.left, targetSum, new List<int>(lst), mainLst);
+        if (root.right != null)
+            PathSum(root.right, targetSum, new List<int>(lst), mainLst);
+    }
+
+}
+public class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 5, 4, 8, 11, 13, 4, 7, 2, 5, 1 };
+        TreeNodeService service = new TreeNodeService();
+
+        TreeNode treeNode =
+                    new TreeNode(5,
+                    new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))),
+                    new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(5), new TreeNode(1)))); ;
+        var res = service.PathSum(treeNode, 22);
+        Console.WriteLine(res);
+    }
+}
