142f4336960a8933cd58b2741ba9900494080d26
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-08-25 06:45:17 +0530

    DijkistraAlgo

---
+++ AllCoreFiles/CSharp/WeightedGraphWorking/Extention.cs
@@ -0,0 +1,27 @@
+﻿/*
+
+    Dijkstras algorithm 
+        is used only to find shortest path.
+
+    Minimum Spanning tree (Prim's or Kruskal's algorithm) 
+        get minimum egdes with minimum edge value.
+
+    Prim [ w(u,v) ] and Dijkstra [ w(u,v) + u.key ] 
+        algorithms are almost the same, except for the "relax function".
+
+    GMST and MSTPrim
+        MSTPrim ha GMST ahe
+ 
+*/
+namespace GraphAlgo
+{
+    public static class Extention
+    {
+        public static VerticleVertexNode PopMin(this List<VerticleVertexNode> priorityQueue)
+        {
+            var min = priorityQueue.OrderBy(p => p.Distance).First();
+            priorityQueue.Remove(min);
+            return min;
+        }
+    }
+}
