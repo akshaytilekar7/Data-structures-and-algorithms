489ac083f398ee01a631a41f91fe4a27b00b282b
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-09-14 12:20:33 +0530

    RB Tree code strcuture

---
+++ AllCoreFiles/CSharp/RedBlackTree/Models.cs
@@ -0,0 +1,24 @@
+﻿namespace AllCoreFiles.CSharp.RedBlackTree
+{
+    public enum Color
+    {
+        RED = 0,
+        BLACK
+    };
+
+    public class RbNode
+    {
+        public int Data;
+        public Color Color; // Added extra 
+        public RbNode Left;
+        public RbNode Right;
+        public RbNode Parent;
+    };
+
+    public class RbTree
+    {
+        public RbNode Root;
+        public RbNode Nil; // Added extra for Sentinal node
+        public int Count;
+    };
+}
---
+++ AllCoreFiles/CSharp/RedBlackTree/RBTServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllCoreFiles.CSharp.RedBlackTree
+{
+    public class RBTServices
+    {
+        public readonly RbTree p_rb_tree;
+        public RbNode GetRBTreeNode(int data, Color color, RbNode p_nil)
+        {
+            RbNode p_rb_node = new RbNode();
+            p_rb_node.Data = data;
+            p_rb_node.Color = color;
+            p_rb_node.Left = p_nil;
+            p_rb_node.Right = p_nil;
+            p_rb_node.Parent = p_nil;
+            return p_rb_node;
+        }
+
+        public RBTServices()
+        {
+            p_rb_tree = new RbTree();
+            p_rb_tree.Root = null;
+            p_rb_tree.Nil = GetRBTreeNode(-1, Color.BLACK, null);
+        }
+    }
+}
