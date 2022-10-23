f7aad405a2b2ecf0523b8f6b6371d9fa41382050
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-10-23 11:40:16 +0530

    Create RodCut.cs

---
+++ AllCoreFiles/CSharp/DP/RodCut.cs
@@ -0,0 +1,51 @@
+﻿namespace CSharp.DP
+{
+    public class RodCut
+    {
+        public static void Main()
+        {
+            long i;
+            long input = Convert.ToInt32(10);
+            long length = input + 1;
+            long[] arr = new long[length];
+
+            for (i = 1; i < length; ++i)
+            {
+                if (i % 2 == 0)
+                    arr[i] = 2 * i + 1;
+                else
+                    arr[i] = 2 * i - 2;
+            }
+
+            Console.WriteLine("Showing p:");
+            for (i = 1; i < length; ++i)
+                Console.Write("arr[" + i + "]:" + arr[i] + "\n");
+
+            for (i = 1; i < length; ++i)
+            {
+                long rs = ComputeRod(i, arr);
+                Console.WriteLine("r[" + i + "]:" + rs + "\n");
+            }
+
+            Console.WriteLine("END $$$$$$$$$$$$$$$$$$");
+        }
+
+        public static long ComputeRod(long index, long[] arr)
+        {
+            long i;
+            long arrVal = arr[index];
+            if (index == 1)
+                return arr[1];
+
+            for (i = 1; i < index / 2; ++i)
+            {
+                long ri = ComputeRod(i, arr) + ComputeRod(index - i, arr);
+                if (ri > arrVal)
+                    arrVal = ri;
+            }
+
+            return arrVal;
+        }
+
+    }
+}
