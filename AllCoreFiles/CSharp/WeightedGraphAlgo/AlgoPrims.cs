2fd5f7f945301e737e0125bfe9bfe33e56135edc
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-10-24 09:32:23 +0530

    Dijkistra and Prims

---
+++ AllCoreFiles/CSharp/WeightedGraphAlgo/AlgoPrims.cs
@@ -0,0 +1,80 @@
+﻿namespace CSharp.WeightedGraphAlgo
+{
+    public class AlgoPrims
+    {
+        private readonly GraphManagement _graphManagement;
+        private const int Infinity = 1000;
+        public AlgoPrims(GraphManagement graphManagement)
+        {
+            _graphManagement = graphManagement;
+        }
+
+        public void Prims(int data)
+        {
+            var pq = InitilizeSingleSource(data);
+
+            while (pq.Count != 0)
+            {
+                var src = pq.PopMin();
+                var traverse = src.LinkList.Next;
+
+                // src sagle neghbours / adj list in PQ madhe ahe - te CROSSING EDGES thartat
+                // and we select least weight edge - which is safe edge - GMST
+                while (traverse != src.LinkList)
+                {
+                    if (pq.Any(x => x.Vertex == traverse.Vertex)) // NEW
+                    {
+                        var dest = _graphManagement.SearchVertexNode(_graphManagement._graph.Root, traverse.Vertex);
+                        Relax(src, dest, traverse.Weight);
+                    }
+                    traverse = traverse.Next;
+                }
+            }
+            pq.Clear();
+        }
+
+        private static void Relax(VertexNode src, VertexNode dest, int weight)
+        {
+            if (dest.Distance > weight) // NEW 
+            {
+                dest.Predecessor = src;
+                dest.Distance = weight;
+            }
+        }
+
+        private List<VertexNode> InitilizeSingleSource(int src)
+        {
+            List<VertexNode> pq = new List<VertexNode>();
+            var srcVertex = _graphManagement.SearchVertexNode(_graphManagement._graph.Root, src);
+
+            var traverse = _graphManagement._graph.Root.Next;
+            while (traverse != _graphManagement._graph.Root)
+            {
+                traverse.Distance = Infinity;
+                traverse.Predecessor = null;
+                pq.Add(traverse);
+                traverse = traverse.Next;
+            }
+            srcVertex.Distance = 0;
+            return pq;
+        }
+        public void PrintPrims(int src)
+        {
+            var srcVertex = _graphManagement.SearchVertexNode(_graphManagement._graph.Root, src);
+
+            Console.WriteLine("Vertices in spanning tree");
+            Console.Write("[START]<.");
+            for (var traverse = _graphManagement._graph.Root.Next; traverse != _graphManagement._graph.Root; traverse = traverse.Next)
+                Console.Write("[" + traverse.Vertex + "]<.");
+            Console.WriteLine("[END]");
+
+            Console.Write("Edges in spanning tree");
+            Console.Write("[START]<.");
+            for (var traverse = _graphManagement._graph.Root.Next; traverse != _graphManagement._graph.Root; traverse = traverse.Next)
+                if (traverse != srcVertex)
+                    Console.Write("[" + traverse.Vertex + "-" + traverse.Predecessor.Vertex + "]<.");
+            Console.WriteLine("[END]");
+        }
+
+    }
+}
---
+++ AllCoreFiles/CSharp/WeightedGraphAlgo/GraphModel.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharp.WeightedGraphAlgo
+{
+    public class Graph
+    {
+        public VertexNode Root { get; set; }
+        public int TotalVertex { get; set; }
+        public int TotalEdge { get; set; }
+
+    }
+
+    public class VertexNode
+    {
+        public int Vertex { get; set; }
+        public VertexNode Prev { get; set; }
+        public VertexNode Next { get; set; }
+
+        public LinkList LinkList { get; set; }
+
+        public int Distance { get; set; }
+        public VertexNode Predecessor { get; set; }
+    }
+
+    public class LinkList
+    {
+        public int Vertex { get; set; }
+        public LinkList Prev { get; set; }
+        public LinkList Next { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class Edge
+    {
+        public int VertexStart;
+        public int VertexEnd;
+        public int Weight;
+    }
+}
