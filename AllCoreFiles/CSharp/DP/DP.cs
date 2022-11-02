abbbabaa93661d76328698ffb9376f26cb215621
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-11-02 20:50:34 +0530

    Update DP.cs

--- AllCoreFiles/CSharp/DP/DP.cs
+++ AllCoreFiles/CSharp/DP/DP.cs
@@ -1,6 +1,9 @@
 ﻿// https://www.google.co.in/books/edition/Dynamic_Programming_for_Coding_Interview/P-jtDQAAQBAJ?hl=en&gbpv=1&dq=dynamic+programming+for+interviews+pdf&printsec=frontcover
 // Dynamic Programming for Coding Interviews A Bottom-Up approach to problem solving
 
+
+// discrete maths by rosen
+
 // 22 23 holiday october
 // 12 13 / 19 20 November DSA END
 // DP
