489344cf8bf5cec6db91146547ca92a54e5afb6d
Akshay Tilekar <akshaytilekar7@gmail.com>
2022-11-10 20:39:27 +0530

    Update DpBook.cs

--- AllCoreFiles/CSharp/DP/DpBook.cs
+++ AllCoreFiles/CSharp/DP/DpBook.cs
@@ -34,6 +34,33 @@ Memory allocation
     4.  Heap 
             -   dyanamic / runtime memory. malloc/calloc/realloc
             -   Heap and Stack segment share common area and grows toward each other
+
+    static int x = strlen("Hello")
+        -   compile time error
+        -   static varaible cannot init by return va;ue of function
+        -   coz static variable are init at compile time but fun() cannot be called at load time
+        -   soln    - static int x ; x =  strlen("Hello")
+        
+        void fun(){
+            int a = 5;
+            static int b = a;
+            print(a);
+        }
+        -   error, coz static variable are init ar load time and here a is not initilize at load time 
+            it will init when fun() gets called
+
+        -   ie 
+            - load time varaible cannot init with local variables
+            - global and static variable only init with constants
+    
+      3. Optimal substructure
+        -   is soln to size of n element based on optimal soln to same problem of smaller size
+        -   lets say we have cities Delhi - Pune - Mumbai
+        -   id shortest path from Delhi to Mumbai passing the Pune
+            then its a sum of shortest path from - Delhi to Pune & Pune to Delhi 
+        -   we can write recursive formula for a soultion to a problem of finding shortest path
+    
+
         
 
 */ 
