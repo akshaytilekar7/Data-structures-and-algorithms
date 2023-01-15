a1fe92c9af271d2fe5e2e851c2e4bbce2bb6fdfa
Akshay Tilekar <akshaytilekar7@gmail.com>
2023-01-15 09:21:59 +0530

    notes

---
+++ SLL/Notes.cs
@@ -0,0 +1,15 @@
+﻿/*
+
+    SLL
+        -  only next pointer and no baclword and NO circular
+    SCLL
+     -  worst LL as dosent add any advantages
+     -  one adv - from last node we can easily go to first node
+     -  but its useless as we already have an pointer on first node
+    DLL
+       -   move forword and backword
+    DCLL 
+        -  BEST move forword and backword + circular
+
+
+*/
