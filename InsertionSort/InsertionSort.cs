e66312e065569fb797218adcdc513ce5c0e02e94
Akshay Tilekar <akshaytilekar7@gmail.com>
2023-01-16 15:49:59 +0530

    InsertionSort

---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿namespace InsertionSort
+{
+    public class InsertionSort
+    {
+        public void InsertionSort1(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int pivot = array[i];
+                int j = i - 1;
+                while (j > 0 && array[j] > pivot)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = pivot;
+            }
+        }
+
+        public void Print(int[] array)
+        {
+            Console.WriteLine();
+            foreach (var item in array)
+                Console.Write(" [{0}] ", item);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InsertionSort
+{
+    public static class Client
+    {
+        public static void Main()
+        {
+            InsertionSort sort = new InsertionSort();
+            int[] arr1 = { 980, 34, 64, 8, 2, 43, 94, 28, -98, 43, 2, 32, 100, 50, 165, 20, 46, 140, 145, 5, 35, -65, 500, 89, 14, 12, 12 };
+            sort.InsertionSort1(arr1);
+            sort.Print(arr1);
+        }
+    }
+}
